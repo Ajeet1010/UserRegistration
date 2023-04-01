8c88c29e8232fdf4bfe5a618b90de5a0fcdf8372
Ajeet <ajeetpal101999@gmail.com>
2023-04-01 23:48:52 +0530

    [Ajeet] First Name of user.

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("*****  User Registartion using REGEX pattern");
+            Registartion registartion = new Registartion();
+            registartion.FirstName();
         }
     }
 }
---
+++ UserRegistration/Registartion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    // UC1- Check validation for USER's FIRST NAME.
+    internal class Registartion
+    {
+        const string FIRST_NAME = "^[A-Z]{1}[a-z]{1,}$";
+        public void FirstName()
+        {
+            Console.Write("Enter the user's first name:  ");
+            string input = Console.ReadLine();
+            if (Regex.IsMatch(input, FIRST_NAME))
+                Console.WriteLine("User's First Name - " + input);
+            else
+                Console.WriteLine("First letter should be capital");
+        }
+    }
+}
